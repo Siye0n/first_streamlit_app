7b2e7b482690c62a766bd0ce62131c983105a763
Siye0n <mathieu.berruet@gmail.com>
2022-07-29 11:12:02 +0200

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & BlueBerry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-range egg')
+streamlit.text('🥣 Omega 3 & BlueBerry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-range egg')
+streamlit.text('🥑🍞 Avocado Toast')
