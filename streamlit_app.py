49d0b95b954c0423776d2fb99261de674b043148
Siye0n <mathieu.berruet@gmail.com>
2022-07-29 23:49:34 +0200

    Update streamlit_app.py
    correctoipn

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 import snowflake.connector
@@ -45,3 +44,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
